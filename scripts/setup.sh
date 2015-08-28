f250cb7908756a09e678fa9288afe11c02b3b90d
bbordoloi <bijoy_bord@yahoo.com>
2015-08-27 19:23:03 -0700

    test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gunzip ./test-app-1.0-SNAPSHOT.tgz
-tar -xvf ./test-app-1.0-SNAPSHOT.tar
-chmod +x ./test-app-1.0-SNAPSHOT/bin/test-app
-./test-app-1.0-SNAPSHOT/bin/test-app -Dconfig.file=/home/ec2-user/production.conf
+gunzip ./play-app/test-app-1.0-SNAPSHOT.tgz
+tar -xvf ./play-app/test-app-1.0-SNAPSHOT.tar
+chmod +x ./play-app/test-app-1.0-SNAPSHOT/bin/test-app
+./play-app/test-app-1.0-SNAPSHOT/bin/test-app -Dconfig.file=/home/ec2-user/production.conf
