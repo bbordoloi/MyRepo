3758eabfd06f91e6414f85c49c7246a5288f0b81
bbordoloi <bijoy_bord@yahoo.com>
2015-08-31 11:24:15 -0700

    test

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 kill `cat /home/ec2-user/play-app/test-app-1.0-SNAPSHOT/RUNNING_PID`
-rm -rf /home/ec2-user/play-app/test-app-1.0-SNAPSHOT.tgz
-rm -rf /home/ec2-user/play-app/scripts
+if [ -d /home/ec2-user/play-app.bak/ ]
+then
+  rm -rf /home/ec2-user/play-app.bak/
+fi
+mv /home/ec2-user/play-app /home/ec2-user/play-app.bak
