7ec3f28cefc53001ee348b61002485dfc8704551
bbordoloi <bijoy_bord@yahoo.com>
2015-08-31 11:09:07 -0700

    test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/home/ec2-user/play-app/test-app-1.0-SNAPSHOT/bin/test-app -Dconfig.file=/home/ec2-user/production.conf &
+/home/ec2-user/play-app/test-app-1.0-SNAPSHOT/bin/test-app & # -Dconfig.file=/home/ec2-user/production.conf &
